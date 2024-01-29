0abfd46c5f3fffb3bb4991d5bbb45e0a31887ad9
rogacky11 <rogacky507@gmail.com>
2024-01-29 16:46:50 +0100

    new views

---
+++ MgazineInterface/Models/OrderHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MgazineInterface.Models
+{
+    class OrdersHelper
+    {
+        public OrdersHelper() { }
+
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? ClientId { get; set; }
+        public DateTime? Date { get; set; }
+        public int? StatusId { get; set; }
+        public decimal? TotalValue { get; set; }
+        public int? TypeId { get; set; }
+
+    }
+}
